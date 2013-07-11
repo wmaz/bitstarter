3a0d5a51dfaaf813308f49e236d518aa0f2e4c6f
Wojciech Mazur <wmaz@op.pl>
2013-07-11 22:33:16 +0200

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fbuf=fs.readFileSync('index.html');
+      
+  response.send(fbuf.toString());
 });
 
 var port = process.env.PORT || 5000;
