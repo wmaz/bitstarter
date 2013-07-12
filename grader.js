2af6ef377d880e374c324e8eaecdc9247e46d0f5
Wojciech Mazur <wmaz@op.pl>
2013-07-12 16:58:38 +0200

    add url parsing to grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require ('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +36,7 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -64,11 +66,23 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_address>','Url to check',function(infile) {return infile.toString()} )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+//added to select only 1 option (and removed defaults from file otherwise conditon would be always true)
+ if (program.file && program.url) {
+     console.log('Only one of the options --url and --file may be selected at a time');
+     process.exit(1);
+  }
+  if (program.file) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+  }
+  if (program.url) {
+      console.log(program.url);
+  }  
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
